bb5ee7ee72bb6d26d7e3d7db4a65e00d8f1984e3
AlexViti <alexviti12@gmail.com>
2022-03-09 14:58:56 +0100

    Add cards w/o style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* HTML ELEMENTS */
 const filterSelect = document.getElementById('filter');
+const cardContainer = document.querySelector('.card-container');
 
 const iconsTypeArr = [];
 
@@ -16,4 +17,19 @@ iconsTypeArr.forEach(type => {
 	option.value = type;
 	option.innerHTML = type;
 	filterSelect.append(option);
-})
+});
+
+icons.forEach(icon => renderCard(icon));
+
+function renderCard(icon) {
+	const card = document.createElement('div');
+	card.classList.add('card');
+	const iconEle = document.createElement('i');
+	iconEle.classList = `${icon.family} ${icon.prefix}${icon.name}`;
+	iconEle.style.color = icon.color;
+	card.innerHTML = `
+		${iconEle.outerHTML}
+		<div>${icon.name}</div>
+	`;
+	cardContainer.append(card);
+}
